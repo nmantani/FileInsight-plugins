2b065ad4f6ddbe8505d6d8e9397983769fc8b249
Nobutaka Mantani <2285962+nmantani@users.noreply.github.com>
2021-04-23 01:42:33 +0900

    Add capability to save selected color to colorchooser.json.

--- plugins/Operations/colorchooser.py
+++ plugins/Operations/colorchooser.py
@@ -23,20 +23,53 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import collections
+import json
+import os
 import sys
 import tkinter
 import tkinter.colorchooser
+
+config_file_name = "colorchooser.json"
+
+# Load config file
+if os.path.exists(config_file_name):
+    try:
+        f = open(config_file_name, "r")
+        config = json.load(f, object_pairs_hook=collections.OrderedDict)
+        f.close()
+    except:
+        tkinter.messagebox.showerror("Error:", message="Failed to load %s ." % os.path.abspath(config_file_name))
+else:
+    # Set default color
+    config = collections.OrderedDict()
+    config["color"] = (255, 128, 128)
+
+    # Create new config file
+    f = open(config_file_name, "w")
+    json.dump(config, f, indent=4)
+    f.close()
 
 root = tkinter.Tk()
 
 root.attributes("-topmost", True) # Set root window to topmost to make color chooser dialog modal
 root.withdraw() # Hide root window
 
-color = tkinter.colorchooser.askcolor(color=(255, 128, 128))
+color = tkinter.colorchooser.askcolor(color=tuple(config["color"]))
 
 if color[1] == None:
     sys.exit(1)
 else:
     print(color[1])
-    sys.exit(0)
 
+    red = int(color[1][1:3], 16)
+    green = int(color[1][3:5], 16)
+    blue = int(color[1][5:7], 16)
+    config["color"] = (red, green, blue)
+
+    # Update config file
+    f = open(config_file_name, "w")
+    json.dump(config, f, indent=4)
+    f.close()
+
+    sys.exit(0)
