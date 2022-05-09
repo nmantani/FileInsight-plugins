0bc544bc6ababa721102b184c13a0e5e66a63a44
Nobutaka Mantani <2285962+nmantani@users.noreply.github.com>
2022-05-09 21:53:20 +0900

    Remove import of unused modules

--- plugins/Operations/Visualization/bitmap_view.py
+++ plugins/Operations/Visualization/bitmap_view.py
@@ -25,12 +25,9 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import binascii
 import os
-import re
 import sys
 import threading
-import time
 import tkinter
 import tkinter.ttk
 
